f22e59693278a6b7f8e91f6a2d2aa46890631bb4
vengatesan-blisslogix <vengatesan@blisslogix.com>
2016-03-29 12:23:08 +0530

    irst

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+file = CSV.foreach("/data/tn_dist.csv", headers:true) do |row|
+
+#@find_dist = DistrictList.find_by_name("#{row[2].strip}")
+puts"--qqqq----#{row[0].strip}----#{row[1].strip}---"
+@dist = DistrictList.new
+@dist.name = row[1].strip
+@dist.save 
+
+end
+
+
+file = CSV.foreach("/data/tn.csv", headers:true) do |row|
+
+@find_dist = DistrictList.find_by_name("#{row[2].strip}")
+puts"--qqqq----#{row[0].strip}----#{row[1].strip}--#{@find_dist.id}--"
+@area = Area.new
+@area.name = row[1].strip
+@area.district_list_id = @find_dist.id
+@area.save
+
+end
